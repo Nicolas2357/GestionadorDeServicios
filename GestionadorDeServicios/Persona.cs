a0b8900e202c17404d5e58579b81af31f1be73a8
Nicolas Ponce Carmona <poncecarmonanicolas09@gmail.com>
2026-02-03 18:16:44 -0600

    Primer commit - estructura inicial del proyecto

---
+++ GestionadorDeServicios/Persona.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionadorDeServicios
+{
+    internal class Persona
+    {
+        // test commit
+    }
+
+}
